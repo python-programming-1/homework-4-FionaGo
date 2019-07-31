3ae49589e2e32d2890aacf5993189383e5a54957
Fiona Li <fiona.li@ticketmaster.com>
2019-07-31 08:48:25 -0700

    added scoring

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -11,20 +11,35 @@ def player_move():
 
     computer = random.choice(['r', 's', 'p'])
 
+    player_score = 0
+    computer_score = 0
+
     if (player == 'r' and computer == 's') or \
         (player == 's' and computer == 'p') or \
         (player == 'p' and computer == 'r'):
 
-         print('you win!')
+        player_score += 1
+        computer_score = computer_score
+
+        print('you win!\n', 'player score:', player_score, 'computer score:', computer_score)
 
     elif (player == 's' and computer == 'r') or \
         (player == 'r' and computer == 'p') or \
         (player == 'p' and computer == 's'):
 
-         print('you lose!')
+        computer_score += 1
+        player_score = player_score
+
+        print('you lose!\n', 'player score:', player_score, 'computer score:', computer_score)
 
     elif player == computer:
-            print('draw!')
+
+        computer_score = computer_score
+        player_score = player_score
+
+        print('draw!\n', 'player score:', player_score, 'computer score:', computer_score)
+
+    return player_score, computer_score
 
 if __name__ == "__main__":
     main()
